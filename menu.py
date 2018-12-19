33e99938ff28071d16e906397ea2f4d7c0610d00
Bryce McDonnell <bryce@bridgetownint.com>
2018-12-18 21:52:06 -0500

    works for breakfast and lunch

---
+++ menu.py
@@ -0,0 +1,41 @@
+"""
+This is a place to stuff breakfast and lunch data
+"""
+
+BREAKFAST = {
+    "2018-12-17": ["Apple Muffin and cheese stick", "Fresh Fruit"],
+    "2018-12-18": ["Turkey Sausage and Egg Sandwich", "Yogurt"],
+    "2018-12-19": ["Pancakes and Ham", "Oatmeal"],
+    "2018-12-20": ["Bagel Sticks", "Yogurt"],
+    "2018-12-21": ["Cheese Omelet and English muffin", "Oatmeal"],
+}
+
+LUNCH = {
+    "2018-12-18": "Hamburgers and Cheeseburgers",
+    "2018-12-19": "Chickpea Tagine",
+    "2018-12-20": "Chicken Dumplings and fried rice, with cookie",
+    "2018-12-21": "Pizza",
+    "2019-01-02": "Hamburgers and Cheeseburgers",
+    "2019-01-03": "Bite size Tacos",
+    "2019-01-04": "Pizza",
+    "2019-01-07": "Spinach and Cheese Quesadillas",
+    "2019-01-08": "Hamburgers and Cheeseburgers",
+    "2019-01-09": "Chickpea Tagine",
+    "2019-01-10": "Chicken Dumplings and fried rice, with cookie",
+    "2019-01-11": "Pizza",
+    "2019-01-14": "Veggie Taco",
+    "2019-01-15": "Hamburgers and Cheeseburgers",
+    "2019-01-16": "Turkey, Mashed potatoes and muffin",
+    "2019-01-17": "Pasta Fagioli and cookie",
+    "2019-01-18": "Pizza",
+    "2019-01-21": "No School",
+    "2019-01-22": "Hamburgers and Cheeseburgers",
+    "2019-01-23": "Zucchini Parmigian w/ pasta and cookie",
+    "2019-01-24": "Bite size Tacos",
+    "2019-01-25": "Pizza",
+    "2019-01-28": "Spinach and Cheese Quesadillas",
+    "2019-01-29": "Hamburgers and Cheeseburgers",
+    "2019-01-30": "Chickpea Tagine",
+    "2019-01-31": "Chicken Dumplings and fried rice, with cookie",
+    "2019-02-01": "Pizza",
+}
